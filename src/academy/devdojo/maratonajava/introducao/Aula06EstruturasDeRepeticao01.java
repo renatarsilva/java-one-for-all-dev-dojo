97ae4958081ffa8221017ce1c4ae0514e50d9ecb
Renata <renata.rodriguesdasilva@outlook.com>
2025-05-12 15:32:31 -0300

    feat update estrutura de repeticao 02

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
@@ -4,23 +4,17 @@ public class Aula06EstruturasDeRepeticao01 {
     public static void main(String[] args) {
 
         int count = 0;
-
-        // while
-//        while (count < 10){
+//        while(count < 10){
 //            System.out.println(++count);
 //        }
-
-        //do-while
 //        count = 0;
-//        do {
-//            System.out.println("dento do do-while "+ ++count);
-//        } while (count < 10);
+        do{
+            System.out.println("dentro do do-while " + count++);
+        } while (count < 3);
 
-        // for, para lista de valores, vai de um em um, geralmente a variavel vai dentro da condicao
-        // i comecando de zero, até i menos 10, incrementando de um em um, faca:
-        for (int i=0; i<10; i++){
-            System.out.println("For"+i);
-
+        for(int i=0; i < 10 ; i++){
+            System.out.println("For "+i);
         }
     }
 }
+
