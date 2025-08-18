1c7f1efde156b567eb31d543cabd5676655c6b41
Renata <renata.rodriguesdasilva@outlook.com>
2025-08-18 15:26:07 -0300

    class07 array03

--- src/academy/devdojo/javaOneForAll/introduction/Class07Arrays03_1.java
+++ src/academy/devdojo/javaOneForAll/introduction/Class07Arrays03_1.java
@@ -2,15 +2,29 @@ package academy.devdojo.javaOneForAll.introduction;
 
 public class Class07Arrays03_1 {
     public static void main(String[] args) {
-        int [] numbers = new int [3];
-        int [] numbers2 = {1,2,3,4,5};
-        int [] numbers3 = new int []{5,4,3,2,1,0};
+        int[] numbers = new int[3];
+        int[] numbers2 = {1,2,3,4,5};
+        int[] numbers3 = new int[]{5,4,3,2,1};
 
-        for (int i = 0; i < numbers2.length; i++){
-            System.out.println(numbers2[i]);
+        for (int i =0;i< numbers3.length;i++){
+            System.out.println(numbers3[i]);
         }
-        for (int num: numbers3){
+
+        //foreach
+        for(int num: numbers3){
             System.out.println(num);
         }
+
+//        int num = numbers3[0];
+//        System.out.println(num);
+//
+//        num = numbers3[1];
+//        System.out.println(num);
+//
+//        num = numbers3[2];
+//        System.out.println(num);
+
+
+
     }
 }
